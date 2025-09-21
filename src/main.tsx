b738f8d1a1695ccb7816491e84169301dee70efa
SergWhitey <truesteell@gmail.com>
2025-09-21 14:51:33 +0300

    y

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../components/App/App'
+import App from '../src/components/App/App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'modern-normalize/modern-normalize.css';
 
